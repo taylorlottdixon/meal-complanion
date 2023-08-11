4b1d0e1f134c4e43463c1d64a7ddc2080bd74c3c
taylorlottdixon <136010920+taylorlottdixon@users.noreply.github.com>
2023-08-11 14:54:04 -0500

    add tag class

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,44 @@
 from django.db import models
+from django.urls import reverse
 from djrichtextfield.models import RichTextField
 from django.contrib.auth.models import User
+
+
+COLORS = (
+    ('Wi', 'White')
+    ('LP', 'Light Purple'),
+    ('LB', 'Light Blue'),
+    ('Rd', "Red"),
+    ('Yw', 'Yellow'),
+    ('Gn', 'Green'),
+    ('Or', 'Orange'),
+    ('Pk', 'Pink'),
+    ('Ba', 'Black'),
+    ('Tl', 'Teal'),
+    ('LG', 'Light Grey'),
+    ('DG', 'Dark Grey'),
+    ('DB', 'Dark Blue'),
+    ('Mr', 'Maroon'),
+    ('Gl', 'Gold'),
+    ('DP', 'Dark Purple'),
+)
 
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'Category {self.name}'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(
+        max_length=2,
+        choices=COLORS,
+        default=COLORS[0][0]
+    )
+
 
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
@@ -17,3 +51,12 @@ class Recipe(models.Model):
     ingredients = models.TextField(max_length=500)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    tags = models.ManyToManyField(Tag, related_name='recipes')
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+    
+    def get_absolute_url(self):
+        return reverse("recipe_detail", kwargs={'pk': self.id, "recipe_id": self.id})
