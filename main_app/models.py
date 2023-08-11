4af665e713ec879c909e5d647983727451b8dcbb
taylorlottdixon <136010920+taylorlottdixon@users.noreply.github.com>
2023-08-11 13:54:28 -0500

    add recipe and category models
    also add RichTextField capability for recipe instructions

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from djrichtextfield.models import RichTextField
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
 class Recipe(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=500)
-    
+    prep_time = models.IntegerField(help_text='minutes')
+    cook_time = models.IntegerField(help_text='minutes')
+    servings = models.IntegerField()
+    serving_size = models.CharField(max_length=10)
+    instructions = RichTextField()
+    ingredients = models.TextField(max_length=500)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
