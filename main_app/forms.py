b26f54c19278e0b112938e3ee220c03cd1056b1a
Seoulless25 <k.j.jackson1016@gmail.com>
2023-08-16 12:25:27 -0400

    Update to recipe details

--- main_app/forms.py
+++ main_app/forms.py
@@ -5,3 +5,12 @@ class TagForm(ModelForm):
   class Meta:
     model = Tag
     fields = '__all__'
+
+# class IngredientForm(ModelForm):
+#     Ingredients.objects.all(),
+#     widget=admin.widgets.RelatedFieldWidgetWrapper(
+#            widget=admin.widgets.FilteredSelectMultiple('Ingredients', False),
+#            rel=Post.ingredients.rel,
+#            admin_site=admin.site
+#         ),
+#     required=False,
